32447b5394d5c089ae52d2deef89e8f9f122ffcf
oussama.assassi <oussama.assassi@Transparency-one.com>
2016-09-30 15:00:41 +0200

    change product name

--- Transparency-One/app/src/main/java/com/example/oussa/transparency_one/ProductsService.java
+++ Transparency-One/app/src/main/java/com/example/oussa/transparency_one/ProductsService.java
@@ -13,11 +13,11 @@ public class ProductsService {
     public List<Product> getProductsAvailable()
     {
         List<Product> products = new ArrayList<Product>();
-        products.add(new Product("Own Paella",  R.drawable.primary_icon));
-        products.add(new Product("Own  Marshmallows", R.drawable.manufacturing_icon));
-        products.add(new Product("Own Yogurt Cherry", R.drawable.distribution_icon));
-        products.add(new Product("Own Mayan Drink", R.drawable.primary_icon));
-        products.add(new Product("Own Lakewood Drink", R.drawable.manufacturing_icon));
+        products.add(new Product("Rice",  R.drawable.primary_icon));
+        products.add(new Product("Marshmallows", R.drawable.manufacturing_icon));
+        products.add(new Product("Yogurt Cherry", R.drawable.distribution_icon));
+        products.add(new Product("Milk", R.drawable.primary_icon));
+        products.add(new Product("Lakewood Drink", R.drawable.manufacturing_icon));
         return products;
     }
 
